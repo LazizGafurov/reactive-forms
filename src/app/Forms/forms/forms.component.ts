ccae33fb659df72842a50c1e152b0f67c03d0d2c
Your Name <you@example.com>
2025-02-18 18:43:52 +0500

    init

---
+++ src/app/Forms/forms/forms.component.ts
@@ -0,0 +1,39 @@
+import { NgIf } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-forms',
+  imports: [ReactiveFormsModule, NgIf, MatButtonModule],
+  templateUrl: './forms.component.html',
+  styleUrl: './forms.component.scss',
+})
+export class FormsComponent {
+  register = new FormGroup({
+    firstName: new FormControl('', [
+      Validators.required,
+      Validators.maxLength(3),
+    ]),
+    lastName: new FormControl('', [
+      Validators.required,
+      Validators.maxLength(3),
+    ]),
+    address1: new FormControl('', Validators.required),
+    address2: new FormControl('', Validators.required),
+    city: new FormControl('', Validators.required),
+    region: new FormControl('', Validators.required),
+    postCode: new FormControl('', Validators.required),
+    country: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    phone: new FormControl('', [Validators.required]),
+    patron: new FormControl('', Validators.required),
+    sponsor: new FormControl('', Validators.required),
+  });
+
+  onSubmit() {
+    console.log(this.register.value);
+  }
+  addToCart() {
+    console.log('Item added to cart');}
+}
---
+++ src/app/Pages/home/home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { FormsComponent } from "../../Forms/forms/forms.component";
+
+@Component({
+  selector: 'app-home',
+  imports: [FormsComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+
+}
